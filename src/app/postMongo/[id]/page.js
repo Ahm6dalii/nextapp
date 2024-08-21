ffeea23e9e17d317550d86fb069671779bcd5f0b
Ahmed Ali <ahmedalielian20@gmail.com>
2024-08-21 15:27:05 +0300

    rmo

--- src/app/postMongo/[id]/page.js
+++ src/app/postMongo/[id]/page.js
@@ -10,15 +10,15 @@ const page = async({params}) => {
     // const paras = parseInt(params.id)
     // const router = useRouter()
     
-    const mongoClient= new MongoClient("mongodb://localhost:27017/lab2");
-    const client =await mongoClient.connect();
-    const db=client.db();
-    const news =await db.collection("note").find({}).toArray();
-    const myNews=news.filter(item=>item.id==params.id)
-    console.log(myNews,'ss');
+    // const mongoClient= new MongoClient("mongodb://localhost:27017/lab2");
+    // const client =await mongoClient.connect();
+    // const db=client.db();
+    // const news =await db.collection("note").find({}).toArray();
+    // const myNews=news.filter(item=>item.id==params.id)
+    // console.log(myNews,'ss');
     
   return (<>
- <div className='grid grid-cols-4 gap-4'>
+ {/* <div className='grid grid-cols-4 gap-4'>
 
 {myNews&&<CardImg
           key={myNews[0]?.id}
@@ -26,11 +26,11 @@ const page = async({params}) => {
          content={myNews[0]?.content}
          image={myNews[0]?.image_url}
           id={myNews[0]?.id}
-        />}
+        />} */}
         {/* <button onClick={() => router.replace(`/lib/mongodb/${paras + 1}`)}>next</button>
         <button onClick={() => router.replace(`/lib/mongodb/${paras - 1}`)}>previous</button> */}
 
-</div>
+{/* </div> */}
 </>)
 }
 
--- src/app/postMongo/page.js
+++ src/app/postMongo/page.js
@@ -3,14 +3,14 @@ import React from 'react'
 import CardImg from "@/components/CardImage";
 
 const page = async() => {
-    const mongoClient= new MongoClient("mongodb://localhost:27017/lab2");
-    const client =await mongoClient.connect();
-    const db=client.db();
-    const news =await db.collection("note").find({}).toArray();
-    console.log(news);
+    // const mongoClient= new MongoClient("mongodb://localhost:27017/lab2");
+    // const client =await mongoClient.connect();
+    // const db=client.db();
+    // const news =await db.collection("note").find({}).toArray();
+    // console.log(news);
     
   return (<>
- <div className='grid grid-cols-4 gap-4'>
+ {/* <div className='grid grid-cols-4 gap-4'>
 {news.map((item) => (
 <CardImg
           key={item.id}
@@ -21,7 +21,7 @@ const page = async() => {
         />
       ))}
 
-</div>
+</div> */}
 </>)
   
 }
