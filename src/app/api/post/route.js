ad45e714a322e9aca3c1fb9624eb3bfc35d61a4c
Ahmed Ali <ahmedalielian20@gmail.com>
2024-08-21 15:31:34 +0300

    rmo

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -1,17 +1,18 @@
 import clientConnect from "@/lib/dbConnection";
 
 export async function GET(request) {
-  const client = await clientConnect
-  const db = client.db();
-  const post = await db.collection("note").find({}).toArray();
-  return new Response(JSON.stringify(post), { status: 200 });
+  // const client = await clientConnect
+  // const db = client.db();
+  // const post = await db.collection("note").find({}).toArray();
+  // return new Response(JSON.stringify(post), { status: 200 });
 }
 
 export async function POST(request) {
- console.log(request.nextUrl.searchParams);
-  const data = await request.json();
-  const client = await clientConnect;
-  const db = client.db();
-  await db.collection("note").insertOne(data);
-  return new Response("success", { status: 201 });
+//  console.log(request.nextUrl.searchParams);
+//   const data = await request.json();
+//   const client = await clientConnect;
+//   const db = client.db();
+//   await db.collection("note").insertOne(data);
+//   return new Response("success", { status: 201 });
+// }
 }
--- src/app/lib/mongodb/page.js
+++ src/app/lib/mongodb/page.js
@@ -3,14 +3,14 @@ import React from 'react'
 import CardImg from "@/components/CardImage";
 
 const page = async() => {
-    const mongoClient= new MongoClient("mongodb://localhost:27017/lab2");
-    const client =await mongoClient.connect();
-    const db=client.db();
-    const news =await db.collection("note").find({}).toArray();
-    console.log(news);
+    // const mongoClient= new MongoClient("mongodb://localhost:27017/lab2");
+    // const client =await mongoClient.connect();
+    // const db=client.db();
+    // const news =await db.collection("note").find({}).toArray();
+    // console.log(news);
     
   return (<>
- <div className='grid grid-cols-4 gap-4'>
+ {/* <div className='grid grid-cols-4 gap-4'>
 {news.map((item) => (
 <CardImg
           key={item.id}
@@ -19,9 +19,9 @@ const page = async() => {
          image={item.image_url}
           id={item.id}
         />
-      ))}
+      ))} */}
 
-</div>
+{/* </div> */}
 </>)
   
 }
